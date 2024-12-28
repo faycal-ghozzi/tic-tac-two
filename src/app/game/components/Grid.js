4484643e11ae511a4ba8b8a2c7d11f07b1f10c6a
Faycal Ghozzi <faycal.ghozzi@gmail.com>
2024-12-28 14:09:58 +0100

    grid render

--- src/app/game/components/Grid.js
+++ src/app/game/components/Grid.js
@@ -15,5 +15,32 @@ const Grid = () => {
         setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X');
     };
 
-    
+    return (
+        <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap: '5px' }}>
+            {
+                grid.map((row, i) => 
+                    row.map((cell, j) => (
+                        <div
+                            key={`${i}-${j}`}
+                            onClick={() => handleMove(i, j)}
+                            style={{
+                                width: '50px',
+                                height: '50px',
+                                backgroundColor: cell ? '#ccc' : '#fff',
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                border: '1px solid #000',
+                                cursor: 'pointer',
+                            }}
+                        >
+                            {cell}
+                        </div>
+                    ))
+                )
+            }
+        </div>
+    )
 }
+
+export default Grid;
