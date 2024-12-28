7ea3dfd94a118af7acb619df90f4309b967afffd
Faycal Ghozzi <faycal.ghozzi@gmail.com>
2024-12-28 14:13:07 +0100

    main game page

---
+++ src/app/game/page.js
@@ -0,0 +1,10 @@
+import Grid from "./components/Grid";
+
+export default function Home() {
+    return (
+        <div>
+            <h1>TicTacTWO</h1>
+            <Grid />
+        </div>
+    )
+}
