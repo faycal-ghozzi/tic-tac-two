34b2665cbed14d7aa70cc2c647466ed07de28168
Faycal Ghozzi <faycal.ghozzi@gmail.com>
2025-04-01 21:01:49 +0100

    sticky navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,7 +18,7 @@ export default function Navbar() {
   // };
 
   return (
-    <nav className="w-full bg-gray-800 text-white flex justify-between items-center px-4 py-2">
+    <nav className="sticky top-0 z-50 w-full bg-gray-800 text-white flex justify-between items-center px-4 py-2 shadow-md">
       <h1 className="text-lg font-bold">
         <Link href="/">Tic Tac Two</Link>
       </h1>
