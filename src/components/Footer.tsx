d25e7aae0b772dad6111b75f9361ce142afbeae1
Faycal Ghozzi <faycal.ghozzi@gmail.com>
2025-04-03 10:51:44 +0100

    fixed typo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="w-full mt-12 py-6 bg-whit border-t border-gray-200 text-sm text-gray-900 ">
+    <footer className="w-full mt-12 py-6 bg-white border-t border-gray-200 text-sm text-gray-900 ">
       <div className="flex flex-col items-center space-y-3">
         <p className="flex items-center gap-2">
           <span className="animate-bounce-slow">💻</span>
