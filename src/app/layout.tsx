3c314c8671847289d0cfa11c323ec9d573e36cd3
Faycal Ghozzi <faycal.ghozzi@gmail.com>
2025-04-03 10:47:11 +0100

    color fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-full">
-      <body className="flex flex-col min-h-screen bg-white">
+      <body className="flex flex-col min-h-screen bg-gray-100">
         <div id="particles-bg" className="absolute inset-0 -z-10 pointer-events-none"></div>
           <Analytics />
           <Navbar />
