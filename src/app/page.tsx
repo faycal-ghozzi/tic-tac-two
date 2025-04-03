c8617c17260576379a385f10500e0ab23f3c4877
Faycal Ghozzi <faycal.ghozzi@gmail.com>
2025-04-03 10:22:26 +0100

    changed nested main to div and centered

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,33 +23,35 @@ export default function HomePage() {
   };
 
   return (
-    <main className="flex flex-col flex-grow items-center justify-center px-4 text-black text-center">
+    <div className="flex flex-col flex-grow items-center justify-center px-4 text-black text-center">
+      <div className="flex flex-col items-center">
 
-      <h1 className="text-5xl font-bold mb-4 text-center text-gray-900">
-        Tic Tac <span className="animate-aurora bg-clip-text text-transparent">TWO!</span>
-      </h1>
-      <p className="text-lg text-center text-gray-600 mt-2 max-w-md mx-auto">
-        fewer moves, more strategy.
-      </p>
-      <br />
-      <br />
-      <br />
-      <div className="flex flex-col gap-6 w-full max-w-xs">
-        <Link href="/game/local" className="menu-btn menu-btn-purple">
-          Local Multiplayer
-        </Link>
+        <h1 className="text-5xl font-bold mb-4 text-center text-gray-900">
+          Tic Tac <span className="animate-aurora bg-clip-text text-transparent">TWO!</span>
+        </h1>
+        <p className="text-lg text-center text-gray-600 mt-2 max-w-md mx-auto">
+          fewer moves, more strategy.
+        </p>
+        <br />
+        <br />
+        <br />
+        <div className="flex flex-col gap-6 w-full max-w-xs">
+          <Link href="/game/local" className="menu-btn menu-btn-purple">
+            Local Multiplayer
+          </Link>
 
-        <button
-          onClick={createOnlineGame}
-          className="menu-btn menu-btn-blue text-center"
-        >
-          Create Online Game
-        </button>
+          <button
+            onClick={createOnlineGame}
+            className="menu-btn menu-btn-blue text-center"
+          >
+            Create Online Game
+          </button>
 
-        <Link href="/how-to-play" className="menu-btn menu-btn-green">
-          How to Play
-        </Link>
+          <Link href="/how-to-play" className="menu-btn menu-btn-green">
+            How to Play
+          </Link>
+        </div>
       </div>
-    </main>
+    </div>
   );
 }
