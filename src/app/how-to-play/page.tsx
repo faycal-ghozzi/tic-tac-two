e7eec1a8e72f214d64a27f91c1cee2b15b596b49
Faycal Ghozzi <faycal.ghozzi@gmail.com>
2025-04-03 10:42:44 +0100

    colors change

--- src/app/how-to-play/page.tsx
+++ src/app/how-to-play/page.tsx
@@ -9,7 +9,7 @@ import AnimatedDemo from "../../components/AnimatedDemo";
 
 export default function HowToPlayPage() {
   return (
-    <div className="min-h-screen px-4 md:px-8 py-12 text-gray-800 overflow-y-auto lg:overflow-y-scroll">
+    <div className="min-h-screen px-4 md:px-8 py-12 bg-white text-gray-800 overflow-y-auto lg:overflow-y-scroll">
       <div className="w-full max-w-5xl mx-auto space-y-16">
 
         <h1 className="text-4xl font-bold text-center">How to Play</h1>
