3df0c194ef516e38a7f4b35eb5559cc9e7ed7e8b
Faycal Ghozzi <faycal.ghozzi@gmail.com>
2025-04-06 01:14:16 +0100

    update how to play page

--- src/app/how-to-play/page.tsx
+++ src/app/how-to-play/page.tsx
@@ -24,7 +24,7 @@ export default function HowToPlayPage() {
         <section className="text-center">
         <h2 className="text-2xl font-semibold mb-4">Fading Example</h2>
         <p className="mb-4">
-            Players can only have 2 symbols at once. When placing a third, the oldest one fades away.
+            Unlike an ordinary tic tac toe game, placing a third symbol makes the oldest one fades away.
         </p>
         <div className="flex justify-center">
             <BoardCoreCompact
